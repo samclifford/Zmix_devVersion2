499a24e60d7e20f6c031f96911554165c5af046c
Zoe van Havre <zoevanhavre@gmail.com>
2014-09-15 18:39:45 +1000

    Stupid tiny mistake fixed (extra ')

--- R/Replicated_Zmix_light.R
+++ R/Replicated_Zmix_light.R
@@ -8,7 +8,7 @@
 #' 
 #'   
 #'  go<- ReplicatedZmix_light(3, 1, 20, K=5, iter=400)
-#''
+#'
 #'
 #'
  ReplicatedZmix_light<-function (NumRep,  sim , n, K=10, ...) {
@@ -17,7 +17,6 @@
 	yrep<-lapply(rep(n, NumRep),  function(x) simMe( sim, x))
  	zmixRun<-lapply(yrep, function(x) Zmix_light(x, K,...) )
  	K0s<-data.frame(  "Replicate"=rep(1:NumRep, each= dim(zmixRun[[1]])[1])  , do.call(rbind, zmixRun))
-	
 	K0s<-K0s[,c(1, rev( 2:dim(K0s)[2] )) ]
 	names(K0s)[-1]<- paste( "Chain", c(1:(dim(K0s)[2] -1)), sep="")
 	Ymatrix<-matrix(unlist(yrep), nrow=2*NumRep, byrow=TRUE)[seq(1,2*NumRep, by=2),]  # each row is a y
