7ccbe7c0e26c1de5910edd37a9abd592f89e83be
Sam Clifford <sj.clifford@student.qut.edu.au>
2017-04-12 12:37:49 +1000

    roxygen stuff

--- R/simudZ.R
+++ R/simudZ.R
@@ -1,14 +1,17 @@
-
 #' Simulate Univariate normal Mixture
 #'
 #' ...
-#' @param  Means, covariance, Sample size, weights
+#' @param n The number of samples to draw
+#' @param mu The mean
+#' @param sig the covariance
+#' @param p the probability weights
+#' @param k labels to sample from
 #' @keywords Wishart
 #' @export
 #' @examples
 #' #not run
 
-simudZ <-function(n,mu,sig,p,k){
+simudZ <-function(n, mu, sig, p, k){
   Z = sample(c(1:k),
              n,
              prob = p,
