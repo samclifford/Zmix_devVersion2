ad1c18ea48848c28574cd5f176916ccb344a2a4c
Sam Clifford <sj.clifford@student.qut.edu.au>
2017-04-12 12:35:46 +1000

    simudz

--- R/simudZ.R
+++ R/simudZ.R
@@ -1,4 +1,4 @@
-	 
+
 #' Simulate Univariate normal Mixture
 #'
 #' ...
@@ -9,6 +9,12 @@
 #' #not run
 
 simudZ <-function(n,mu,sig,p,k){
-			Z = sample((1:k),n,prob = p, replace = T)    # randomly select a labelling (Z) from the number of mixtures k with probability p
-			Y = rnorm(n,mean =mu[Z],sd=sqrt(sig[Z]))       # simulate random normal variable given mean of mixture chosen 
-			list(Y = Y, Z = Z)   }
+  Z = sample(c(1:k),
+             n,
+             prob = p,
+             replace = T)    # randomly select a labelling (Z) from the number of mixtures k with probability p
+  Y = rnorm(n,
+            mean =mu[Z],
+            sd=sqrt(sig[Z]))       # simulate random normal variable given mean of mixture chosen 
+  list(Y = Y, Z = Z)   
+}
